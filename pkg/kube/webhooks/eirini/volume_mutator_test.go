c6997ba9a2123ac3cf7d9f0be893754e8ba22274
André Duffeck <aduffeck@suse.com>
2019-04-04 15:56:19 +0200

    Patch eirini app pods to mount the persistent volumes
    Signed-off-by: Ettore Di Giacinto <edigiacinto@suse.com>

--- pkg/kube/webhooks/eirini/volume_mutator_test.go
+++ pkg/kube/webhooks/eirini/volume_mutator_test.go
@@ -66,7 +66,7 @@ var _ = Describe("Volume Mutator", func() {
 		})
 
 		It("does not act if the source_type: APP label is not set", func() {
-			pod := labeledPod("foo", map[string]string{})
+			pod := labeledPod("foo", map[string]string{}, ``)
 			f := generateGetPodFunc(&pod, nil)
 
 			mutator := webhooks.NewVolumeMutator(log, config, manager, setReferenceFunc, f)
@@ -74,15 +74,116 @@ var _ = Describe("Volume Mutator", func() {
 			resp := mutator.Handle(ctx, request)
 			Expect(len(resp.Patches)).To(Equal(0))
 		})
+
+		It("does act if the source_type: APP label is set and one volume is supplied", func() {
+			vcapservices := `{"eirini-persi": [	  {
+		"credentials": {},
+		"label": "eirini-persi",
+		"name": "my-instance",
+		"plan": "hostpath",
+		"tags": [
+			"erini",
+			"kubernetes",
+			"storage"
+		],
+		"volume_mounts": [
+		  {
+			"container_dir": "/var/vcap/data/de847d34-bdcc-4c5d-92b1-cf2158a15b47",
+			"device_type": "shared",
+			"mode": "rw",
+			"device": {
+				"volume_id": "the-volume-id"
+			}
+		  }
+		]
+	  }
+	]
+}`
+
+			pod := labeledPod("foo", map[string]string{"source_type": "APP"}, vcapservices)
+			f := generateGetPodFunc(&pod, nil)
+
+			mutator := webhooks.NewVolumeMutator(log, config, manager, setReferenceFunc, f)
+
+			resp := mutator.Handle(ctx, request)
+			Expect(len(resp.Patches)).To(Equal(2))
+		})
+
+		It("does act if the source_type: APP label is set and 3 volumes are supplied", func() {
+			vcapservices := `{"eirini-persi": [	  {
+		"credentials": {},
+		"label": "eirini-persi",
+		"name": "my-instance",
+		"plan": "hostpath",
+		"tags": [
+			"erini",
+			"kubernetes",
+			"storage"
+		],
+		"volume_mounts": [
+			{
+				"container_dir": "/var/vcap/data/de847d34-bdcc-4c5d-92b1-cf2158a15b47",
+				"device_type": "shared",
+				"mode": "rw",
+				"device": {
+					"volume_id": "the-volume-id1"
+				}
+			},
+			{
+				"container_dir": "/var/vcap/data/de847d34-bdcc-4c5d-92b1-cf2158a15b47",
+				"device_type": "shared",
+				"mode": "rw",
+				"device": {
+					"volume_id": "the-volume-id2"
+				}
+			},
+			{
+				"container_dir": "/var/vcap/data/de847d34-bdcc-4c5d-92b1-cf2158a15b47",
+				"device_type": "shared",
+				"mode": "rw",
+				"device": {
+					"volume_id": "the-volume-id3"
+				}
+			}
+		]
+	  }
+	]
+}`
+
+			pod := labeledPod("foo", map[string]string{"source_type": "APP"}, vcapservices)
+			f := generateGetPodFunc(&pod, nil)
+
+			mutator := webhooks.NewVolumeMutator(log, config, manager, setReferenceFunc, f)
+
+			resp := mutator.Handle(ctx, request)
+			fmt.Printf("%#v\n", resp)
+			Expect(len(resp.Patches)).To(Equal(2))
+			Expect(len(resp.Patches[0].Value.([]interface{}))).To(Equal(3))
+
+			Expect(len(resp.Patches[1].Value.([]interface{}))).To(Equal(3))
+		})
+
 	})
 })
 
 // LabeledPod defines a pod with labels and a simple web server
-func labeledPod(name string, labels map[string]string) corev1.Pod {
+func labeledPod(name string, labels map[string]string, vcapServices string) corev1.Pod {
 	return corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:   name,
 			Labels: labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				corev1.Container{
+					Env: []corev1.EnvVar{
+						corev1.EnvVar{
+							Name:  "VCAP_SERVICES",
+							Value: vcapServices,
+						},
+					},
+				},
+			},
 		},
 	}
 }
